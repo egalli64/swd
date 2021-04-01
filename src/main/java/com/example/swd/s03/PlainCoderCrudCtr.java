a96b0506516ad718756ec8b04c3b96797f5b8216
Emanuele Galli <egalli64@gmail.com>
2021-04-01 16:39:54 +0200

    #1 some delete still missing

--- src/main/java/com/example/swd/s03/PlainCoderCrudCtr.java
+++ src/main/java/com/example/swd/s03/PlainCoderCrudCtr.java
@@ -1,11 +1,15 @@
 package com.example.swd.s03;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.swd.dao.Coder;
 
@@ -13,7 +17,6 @@ import com.example.swd.dao.Coder;
 @RequestMapping("/s03")
 public class PlainCoderCrudCtr {
     private static final Logger log = LogManager.getLogger(PlainCoderCrudCtr.class);
-    private static final Integer TOM_ID = 202;
 
     private PlainCoderCrudRepo repo;
 
@@ -37,24 +40,82 @@ public class PlainCoderCrudCtr {
     }
 
     @GetMapping("/rename")
-    public String rename(Model model) {
+    public String rename(@RequestParam Integer id, Model model) {
         log.traceEntry("rename");
 
-        repo.findById(TOM_ID).ifPresentOrElse(coder -> {
+        repo.findById(id).ifPresentOrElse(coder -> {
             coder.setFirstName(coder.getFirstName() + "e");
             Coder edited = repo.save(coder);
             model.addAttribute("message", "Saved: " + edited);
-        }, () -> model.addAttribute("message", "No coder found with id " + TOM_ID));
+        }, () -> model.addAttribute("message", "No coder found with id " + id));
 
         return "/s03/result";
     }
 
     @GetMapping("/check")
-    public String check(Model model) {
+    public String check(@RequestParam Integer id, Model model) {
         log.traceEntry("check");
 
-        model.addAttribute("message", String.format("Coder %d %sfound", TOM_ID, repo.existsById(TOM_ID) ? "" : "NOT "));
+        model.addAttribute("message", String.format("Coder %d %sfound", id, repo.existsById(id) ? "" : "NOT "));
 
+        return "/s03/result";
+    }
+
+    @GetMapping("/all")
+    public String all(Model model) {
+        log.traceEntry("all");
+
+        model.addAttribute("message", "Found: " + repo.findAll());
+
+        return "/s03/result";
+    }
+
+    @GetMapping("/some")
+    public String some(@RequestParam List<Integer> ids, Model model) {
+        log.traceEntry("some");
+
+        model.addAttribute("message", "Found: " + repo.findAllById(ids));
+
+        return "/s03/result";
+    }
+
+    @GetMapping("/count")
+    public String count(Model model) {
+        log.traceEntry("count");
+
+        model.addAttribute("message", String.format("Found %d coders", repo.count()));
+
+        return "/s03/result";
+    }
+
+    @GetMapping("/deleteById")
+    public String deleteById(@RequestParam Integer id, Model model) {
+        log.traceEntry("deleteById");
+
+        try {
+            repo.deleteById(id);
+            return "redirect:/s03/check?id=" + id;
+        } catch (Exception ex) {
+            model.addAttribute("message", "Can't delete coder " + id);
+            return "/s03/result";
+        }
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam Integer id, Model model) {
+        log.traceEntry("delete");
+
+        Optional<Coder> opt = repo.findById(id);
+        if (opt.isPresent()) {
+            try {
+                repo.delete(opt.get());
+                return "redirect:/s03/check";
+            } catch (Exception ex) {
+                log.warn("Can't delete coder " + id);
+            }
+        }
+
+        model.addAttribute("message", "Can't delete coder " + id);
         return "/s03/result";
     }
 }
