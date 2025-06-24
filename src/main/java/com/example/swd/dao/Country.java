d7950b176136f983dd874e47d47920fdc28b27a7
Emanuele <egalli64@gmail.com>
2025-06-24 21:19:39 +0200

    An entity in relation many-to-one

---
+++ src/main/java/com/example/swd/dao/Country.java
@@ -0,0 +1,54 @@
+package com.example.swd.dao;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Country {
+    @Id
+    @Column(name = "country_id", length = 2)
+    private String id;
+
+    @Column(name = "name", length = 40)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id", referencedColumnName = "region_id")
+    private Region region;
+
+    public Country() {
+    }
+
+    public Country(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Region getRegion() {
+        return region;
+    }
+
+    public void setRegion(Region region) {
+        this.region = region;
+    }
+}
