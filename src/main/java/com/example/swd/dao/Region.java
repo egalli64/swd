9b8576a86c133d36c3b2c5503a8c5ad89cde8ad0
Emanuele <egalli64@gmail.com>
2025-06-24 21:20:41 +0200

    many-to-one support

--- src/main/java/com/example/swd/dao/Region.java
+++ src/main/java/com/example/swd/dao/Region.java
@@ -1,19 +1,28 @@
 package com.example.swd.dao;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Region {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "REGION_ID")
+    @Column(name = "region_id")
     private Integer id;
 
+    @Column(name = "name", length = 25)
     private String name;
+
+    @OneToMany(mappedBy = "region", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Country> countries;
 
     public Region() {
     }
@@ -36,6 +45,14 @@ public class Region {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Country> getCountries() {
+        return countries;
+    }
+
+    public void setCountries(List<Country> countries) {
+        this.countries = countries;
     }
 
     @Override
