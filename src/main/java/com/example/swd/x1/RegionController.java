373dea2391d505837a0bc4d96f14bd3e1ab9a82f
Emanuele <egalli64@gmail.com>
2025-06-25 09:53:54 +0200

    implicit transactional behavior

---
+++ src/main/java/com/example/swd/x1/RegionController.java
@@ -0,0 +1,48 @@
+package com.example.swd.x1;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.swd.dao.Region;
+
+@RestController
+public class RegionController {
+    private final RegionRepository regionRepository;
+
+    public RegionController(RegionRepository regionRepository) {
+        this.regionRepository = regionRepository;
+    }
+
+    /**
+     * Test:
+     * 
+     * <pre>
+     * curl -X GET http://localhost:8080/regions
+     * </pre>
+     * 
+     * @return a JSON with all regions with associated countries
+     */
+    @GetMapping("/regions")
+    public List<Region> getAllRegions() {
+        return regionRepository.findAll();
+    }
+
+    /**
+     * Test:
+     * 
+     * <pre>
+     * curl -X POST http://localhost:8080/regions -H "Content-Type: application/json" -d "{\"name\": \"Down Below\"}"
+     * </pre>
+     * 
+     * @param region the JSON representing the region to be persisted, like: {"name":"Down Below"} 
+     * @return a JSON like: {"id":5,"name":"Down Below","countries":null}
+     */
+    @PostMapping("/regions")
+    public Region createRegion(@RequestBody Region region) {
+        return regionRepository.save(region);
+    }
+}
---
+++ src/main/java/com/example/swd/x1/RegionRepository.java
@@ -0,0 +1,13 @@
+package com.example.swd.x1;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.swd.dao.Region;
+
+@Repository
+public interface RegionRepository extends JpaRepository<Region, Integer> {
+    List<Region> findByName(String name);
+}
