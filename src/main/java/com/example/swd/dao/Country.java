16de52fbe85bd1c571b7e8737860788e6ac718eb
Emanuele <egalli64@gmail.com>
2025-06-25 09:52:36 +0200

    JSON ignore for Jackson

--- src/main/java/com/example/swd/dao/Country.java
+++ src/main/java/com/example/swd/dao/Country.java
@@ -1,5 +1,7 @@
 package com.example.swd.dao;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -16,6 +18,7 @@ public class Country {
     @Column(name = "name", length = 40)
     private String name;
 
+    @JsonIgnore
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "region_id", referencedColumnName = "region_id")
     private Region region;
