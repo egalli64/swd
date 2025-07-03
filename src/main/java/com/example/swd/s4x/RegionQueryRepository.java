4892cf891cdb592b3d61b31ecbefe9c2709fa3b6
Emanuele <egalli64@gmail.com>
2025-07-03 21:27:52 +0200

    repo w/ query

---
+++ src/main/java/com/example/swd/s4x/RegionQueryRepository.java
@@ -0,0 +1,16 @@
+package com.example.swd.s4x;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.example.swd.entity.Region;
+
+public interface RegionQueryRepository extends JpaRepository<Region, Integer> {
+    @Query("SELECT r FROM Region r LEFT JOIN FETCH r.countries WHERE r.name = :name")
+    Optional<Region> findByNameWithCountries(@Param("name") String name);
+
+    Optional<Region> findByName(String name);
+}
