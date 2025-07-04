97c037b6ac671a85679d6bd9ef00c708aa75136d
Emanuele <egalli64@gmail.com>
2025-07-04 15:50:26 +0200

    KeyHolder

--- src/main/java/com/example/swd/m1/s4/JdbcClientRegionRepository.java
+++ src/main/java/com/example/swd/m1/s4/JdbcClientRegionRepository.java
@@ -7,6 +7,8 @@ package com.example.swd.m1.s4;
 
 import org.springframework.dao.DataRetrievalFailureException;
 import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import com.example.swd.m1.entity.Region;
@@ -39,26 +41,24 @@ public class JdbcClientRegionRepository {
     /**
      * Saves a Region entity. The id determines if it is an INSERT (if missing) or
      * an UPDATE. Assuming the DBMS generates the id on its own.
-     * <p>
-     * This simplified version does not return the entity as persisted
-     * 
-     * @param region The Region object to save.
      */
-    public void save(Region region) {
-        if (region.getId() == null) {
-            insert(region);
-        } else {
-            update(region);
-        }
+    public Region save(Region region) {
+        return (region.getId() == null) ? insert(region) : update(region);
     }
 
-    private void insert(Region region) {
+    private Region insert(Region region) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
         jdbc.sql("INSERT INTO region (name) VALUES (:name)") //
                 .param("name", region.getName()) //
-                .update();
+                .update(keyHolder);
+
+        if (keyHolder.getKey() != null) {
+            region.setId(keyHolder.getKey().intValue());
+        }
+        return region;
     }
 
-    private void update(Region region) {
+    private Region update(Region region) {
         int affected = jdbc.sql("UPDATE region SET name = :name WHERE region_id = :id") //
                 .param("name", region.getName()).param("id", region.getId()) //
                 .update();
@@ -66,5 +66,7 @@ public class JdbcClientRegionRepository {
         if (affected == 0) {
             throw new DataRetrievalFailureException("No region found with ID " + region.getId() + " to update.");
         }
+
+        return region;
     }
 }
